f05ac4ee248392682233dce92b667ccdb8ebedae
unknown <rewoly@mail.ru>
2024-06-10 14:41:27 +0900

    for student

--- Server/src/main/java/com/example/Server/controller/AppController.java
+++ Server/src/main/java/com/example/Server/controller/AppController.java
@@ -2,12 +2,13 @@ package com.example.Server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * Контроллер для взаимодействия с приложением JavaFX
+ */
 @RestController
 public class AppController {
     @GetMapping("/application")
     private String getApp() {
         return "Приложение по продаже табачной продукции";
     }
-
 }
--- Server/src/main/java/com/example/Server/controller/WebController.java
+++ Server/src/main/java/com/example/Server/controller/WebController.java
@@ -1,7 +1,9 @@
 package com.example.Server.controller;
 
 import org.springframework.stereotype.Controller;
-
+/**
+ * Контроллер для взаимодействия с клиентами по веб - браузеру
+ */
 @Controller
 public class WebController {
 }
